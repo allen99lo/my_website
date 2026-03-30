0f2443d31764055b110d82febfc9f1b16a286eff
Your Name <you@example.com>
2026-03-30 10:30:23 +0800

    Add excel link

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1>我的第一個 Python 網頁在網路上跑起來了！</h1>"
+def home():
+    return """
+    <html>
+        <head><title>我的班表網頁</title></head>
+        <body>
+            <h1>我的個人網站</h1>
+            <p>歡迎查看我的班表：</p>
+            
+            <a href="/static/schedule.xlsx" download>
+                <button style="padding: 10px; cursor: pointer;">下載最新班表 (Excel)</button>
+            </a>
+            
+        </body>
+    </html>
+    """
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
